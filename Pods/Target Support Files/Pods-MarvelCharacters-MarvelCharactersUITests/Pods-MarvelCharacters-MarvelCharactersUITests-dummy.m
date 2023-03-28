ffddbfa4e0ff8cf5290ce8aaa1eaa105514cc223
vitor-rc1 <vitor_rc@outlook.com>
2023-03-27 21:31:30 -0300

    removed pod files

--- Pods/Target Support Files/Pods-MarvelCharacters-MarvelCharactersUITests/Pods-MarvelCharacters-MarvelCharactersUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MarvelCharacters_MarvelCharactersUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MarvelCharacters_MarvelCharactersUITests
-@end
