421396df11ba71847f349fad60ce2607664ea02c
vitor-rc1 <vitor_rc@outlook.com>
2023-03-30 21:53:58 -0300

    Created VIewCode protocol

---
+++ MarvelCharacters/Scenes/Commons/Protocols/ViewCode.swift
@@ -0,0 +1,15 @@
+//
+//  ViewCode.swift
+//  MarvelCharacters
+//
+//  Created by Vitor Conceicao on 30/03/23.
+//
+
+import Foundation
+
+protocol ViewCode {
+    func buildViewHierarchy()
+    func setupConstraints()
+    func setupAdditionalConfiguration()
+    func setupView()
+}
