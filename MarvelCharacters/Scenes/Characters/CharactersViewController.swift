f2e00671ff7c391d84526844675505ebc47ba500
vitor-rc1 <vitor_rc@outlook.com>
2023-03-30 22:18:23 -0300

    CharacterViewController refactor: added ViewCode protocol, extensions of collection view delegate and data source, and change collection controller to view controller.

--- MarvelCharacters/Scenes/Characters/CharactersViewController.swift
+++ MarvelCharacters/Scenes/Characters/CharactersViewController.swift
@@ -9,29 +9,51 @@ import UIKit
 import SDWebImage
 import CoreData
 
-class CharactersViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class CharactersViewController: UIViewController {
+    // MARK: - Private Properties
+
+    private lazy var collectionViewFlowLayout: UICollectionViewFlowLayout = {
+        let collectionViewFlowLayout = UICollectionViewFlowLayout()
+        collectionViewFlowLayout.scrollDirection = .vertical
+        collectionViewFlowLayout.minimumLineSpacing = 10
+        collectionViewFlowLayout.minimumInteritemSpacing = 10
+        collectionViewFlowLayout.itemSize = CGSize(width: (view.frame.width / 2) - 5, height: (view.frame.width / 2) - 5)
+        return collectionViewFlowLayout
+    }()
+
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
     private var viewModel: CharactersViewModel
     private var characters: [MarvelCharacter] = []
     private var page = 0
 
+    // MARK: - Inits
+
     init(viewModel: CharactersViewModel) {
         self.viewModel = viewModel
-        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+        super.init(nibName: nil, bundle: nil)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.onErrorHandling = errorHandling
         
-        self.view.backgroundColor = .white
-        
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.register(CardCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        addConstraints()
+        setupView()
         loadCharacters(page: page)
     }
     
@@ -43,74 +65,69 @@ class CharactersViewController: UICollectionViewController, UICollectionViewDele
         let controller = UIAlertController(title: "An error occured", message: message, preferredStyle: .alert)
         controller.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
         DispatchQueue.main.sync {
-            self.present(controller, animated: true, completion: nil)
+            present(controller, animated: true, completion: nil)
         }
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        self.tabBarController?.navigationItem.title = "Characters"
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        characters.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CardCollectionViewCell
-        
-        if (indexPath.row == characters.count - 1 ) {
-            page += 1
-            loadCharacters(page: page)
-        }
-        
-        let character = characters[indexPath.row]
-        cell.buildCell(character)
-        
-        return cell
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let character = characters[indexPath.row]
-        let characterDetailsVC = CharacterDetailsViewController(collectionViewLayout: UICollectionViewFlowLayout())
-        characterDetailsVC.character = character
-        navigationController?.pushViewController(characterDetailsVC, animated: true)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let widhtCell = (collectionView.bounds.width/2.0) - 5.0
-        let heightCell = widhtCell
-        
-        return CGSize(width: widhtCell, height: heightCell)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets.zero
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-    private func addConstraints() {
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 5),
-            self.view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor, constant: 5),
-            collectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            self.view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor, constant: 20),
-        ])
+        tabBarController?.navigationItem.title = "Characters"
     }
     
     private func loadCharacters(page: Int) {
         
         viewModel.fetchCharacters(page: page) { characters in
             self.characters.append(contentsOf: characters)
-            DispatchQueue.main.sync {
-                self.collectionView.reloadData()
+            DispatchQueue.main.sync { [weak self] in
+                self?.collectionView.reloadData()
             }
         }
+    }
+}
+
+extension CharactersViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        characters.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CardCollectionViewCell
+
+        if (indexPath.row == characters.count - 1 ) {
+            page += 1
+            loadCharacters(page: page)
+        }
+
+        let character = characters[indexPath.row]
+        cell.buildCell(character)
+
+        return cell
+    }
+}
+
+extension CharactersViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let character = characters[indexPath.row]
+        let characterDetailsVC = CharacterDetailsViewController(collectionViewLayout: UICollectionViewFlowLayout())
+        characterDetailsVC.character = character
+        navigationController?.pushViewController(characterDetailsVC, animated: true)
+    }
+}
+
+extension CharactersViewController: ViewCode {
+    func buildViewHierarchy() {
+        view.addSubview(collectionView)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
+
+    func setupAdditionalConfiguration() {
+        view.backgroundColor = .white
     }
 }
